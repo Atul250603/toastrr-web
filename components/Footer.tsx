8eaa23f590e6bcc78e979b50a48311b159a759e8
Atul Goyal <atul250603@gmail.com>
2025-03-02 14:18:47 +0530

    update changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <div className="my-4">
             <span>
-                Made with ❤️ By <a href="https://x.com/AtulGoyal383989" target="_blank" className="underline cursor-pointer">Atul</a>
+                Made with ❤️ By <a href="https://x.com/AtulGoyal383989" target="_blank" className="underline cursor-pointer">Atul Goyal</a>
             </span>
         </div>
     )
